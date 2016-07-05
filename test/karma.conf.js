726459da2385eba7dc37bedc9d5b7d591fadd5ef
Pavel Maslov <pavel.masloff@gmail.com>
2016-07-05 11:53:21 +0200

    snarl wiredep bug FIXED

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function(config) {
       'bower_components/angularUtils-pagination/dirPagination.js',
       'bower_components/angular-ui-select/dist/select.js',
       'bower_components/ngprogress/build/ngprogress.min.js',
-      'bower_components/Snarl/dist/snarl.min.js',
+      'bower_components/snarl/dist/snarl.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
